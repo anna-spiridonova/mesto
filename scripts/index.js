a290f491479fd173365232f86de661e2e6e8d63b
Anna Spiridonova <spiridonova.aniutka@yandex.ru>
2022-12-21 22:04:00 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,11 @@
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 //попапы
-const popup = document.querySelector('.popup');
 const placePopup = document.querySelector('.popup_type_place');
 const profilePopup = document.querySelector('.popup_type_profile');
 const imagePopup = document.querySelector('.popup_type_image');
+
+//попап картинки
+const image = imagePopup.querySelector('.popup__image');
+const imageTitle = imagePopup.querySelector('.popup__image-title');
 
 //кнопки
 const addButton = document.querySelector('.profile__add-button');
@@ -51,6 +27,8 @@ const imageInput = placeForm.querySelector('.popup__input_type_link');
 const titleInput = placeForm.querySelector('.popup__input_type_title');
 
 const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.cards');
+
 
 //открытие и закрытие попапов
 function openPopup(popup) {
@@ -68,12 +46,13 @@ function submitProfile (evt) {
   closePopup(profilePopup);
 }
 
-//6 карточек при загрузке
-initialCards.forEach(function(item) {
+function newCard(link, name) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-  cardElement.querySelector('.card__image').src = item.link;
-  cardElement.querySelector('.card__title').textContent = item.name;
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').alt = name;
 
   const cardLike = cardElement.querySelector('.card__like-button');
   cardLike.addEventListener('click', function() {
@@ -85,44 +64,30 @@ initialCards.forEach(function(item) {
     cardElement.remove();
   });
 
-  const cardImage = cardElement.querySelector('.card__image');
   cardImage.addEventListener('click', function() {
-    imagePopup.querySelector('.popup__image').src = item.link;
-    imagePopup.querySelector('.popup__image-title').textContent = item.name;
+    image.src = link;
+    imageTitle.textContent = name;
+    image.alt = name;
     openPopup(imagePopup);
   });
 
-  const cardsContainer = document.querySelector('.cards');
-  cardsContainer.append(cardElement);
-});
+  return cardElement
+}
 
+//6 карточек при загрузке
+initialCards.forEach(function(item) {
+  const link = item.link;
+  const name = item.name;
+
+  cardsContainer.append(newCard(link, name));
+});
 
 //добвить карточку
 function addCard(imageInput, titleInput) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const link = imageInput.value;
+  const name = titleInput.value;
 
-  cardElement.querySelector('.card__image').src = imageInput.value;
-  cardElement.querySelector('.card__title').textContent = titleInput.value;
-
-  const cardLike = cardElement.querySelector('.card__like-button');
-  cardLike.addEventListener('click', function() {
-    cardLike.classList.toggle('card__like-button_active');
-  });
-
-  const cardDelete = cardElement.querySelector('.card__delete-button');
-  cardDelete.addEventListener('click', function() {
-    cardElement.remove();
-  });
-
-  const cardImage = cardElement.querySelector('.card__image');
-  cardImage.addEventListener('click', function() {
-    imagePopup.querySelector('.popup__image').src = cardElement.querySelector('.card__image').src;
-    imagePopup.querySelector('.popup__image-title').textContent = cardElement.querySelector('.card__title').textContent;
-    openPopup(imagePopup);
-  });
-
-  const cardsContainer = document.querySelector('.cards');
-  cardsContainer.prepend(cardElement);
+  cardsContainer.prepend(newCard(link, name));
 
   imageInput.value = '';
   titleInput.value = '';
